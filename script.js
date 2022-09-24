006692214f4f78a51a5f70bf4f4a28e6a0234cc6
Swati <swatic019@gmail.com>
2022-09-24 13:55:26 +0530

    layout, css updated

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-let addCardBtn = document.getElementById('add-card-button');
-
-addCardBtn.addEventListener('click', ()=>{
-let addInput = document.getElementById('add-input');
-if(addInput.value == ""){
-  alert('Please add a task!!');
-}
-let list = localStorage.getItem("list");
-if (list == null) {
-  listArray = [];
-} else {
-  listArray = JSON.parse(list);
-}
-listArray.push(addInput.value);
-localStorage.setItem("list", JSON.stringify(listArray));
-
-addInput.value = "";
 showList();
+let addCardBtn = document.getElementById("add-card-button");
+
+addCardBtn.addEventListener("click", () => {
+  let addInput = document.getElementById("add-input");
+  if (addInput.value == "") {
+    alert("Please add a task!!");
+  }
+  let list = localStorage.getItem("list");
+  if (list == null) {
+    listArray = [];
+  } else {
+    listArray = JSON.parse(list);
+  }
+  listArray.push(addInput.value);
+  localStorage.setItem("list", JSON.stringify(listArray));
+
+  addInput.value = "";
+  showList();
 });
 
 function showList(){
- 
+  let list = localStorage.getItem("list");
+  if (list == null) {
+    listArray = [];
+  } else {
+    listArray = JSON.parse(list);
+  }
+  
 }
-
